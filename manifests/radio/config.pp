45d0b379dc4f2c675b4060f6e80f868f6ab3a0ca
Adam Breaux <breauxaj@gmail.com>
2014-03-18 13:12:19 -0400

    radio config

--- manifests/radio/config.pp
+++ manifests/radio/config.pp
@@ -1,5 +1,16 @@
 define graylog2::radio::config (
-
+  $node_id_file = '/etc/graylog2-radio-node-id',
+  $graylog2_server_uri = '',
+  $rest_listen_uri = '',
+  $rest_transport_uri = '',
+  $kafka_brokers = '',
+  $kafka_producer_type = 'async',
+  $kafka_batch_size = '200',
+  $kafka_batch_max_wait_ms = '250',
+  $kafka_required_acks = '0',
+  $processbuffer_processors = '5',
+  $processor_wait_strategy = 'blocking',
+  $ring_size = '1024'
 ){
   file { '/usr/local/graylog2-radio/graylog2-radio.conf':
     ensure  => present,
